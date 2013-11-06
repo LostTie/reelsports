0467847bd1e777ac74cdcf694714f667b4879f0f
Jason Rylance <ecnalyr@gmail.com>
2013-11-06 13:15:11 -0600

    Reverted last commit

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,6 @@ Bundler.require(:default, Rails.env)
 module Reelsports
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
-    
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
