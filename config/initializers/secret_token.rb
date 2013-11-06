d75c675ac465c79ecc3ef73abc1739d1c39ff037
Jason Rylance <ecnalyr@gmail.com>
2013-11-06 11:11:22 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reelsports::Application.config.secret_key_base = 'c619a5d2a29bdf3973d64370aa66e8937783987744dea6b3bdd35385994664dcd22f3dcc750cac7ee29a57a43bb4212f119df2961f1e544e9c177bb84fcd24ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reelsports::Application.config.session_store :cookie_store, key: '_reelsports_session'
