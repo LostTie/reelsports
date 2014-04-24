55e9c16f00fe0e966b927a72f28e0723ee31d284
ecnalyr <ecnalyr@2013mbp.local>
2014-04-24 10:40:53 -0500

    Added a welcome page.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < Spree::StoreController
+    respond_to :html
+  def index
+    @searcher = Spree::Config.searcher_class.new(params)
+    @searcher.current_user = try_spree_current_user
+    @searcher.current_currency = current_currency
+    @products = @searcher.retrieve_products.sample(3)
+  end
+end
