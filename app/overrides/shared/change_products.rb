76153950161bf6f76d4a3cbb37d41b2841add137
ecnalyr <ecnalyr@gmail.com>
2014-05-01 09:04:18 -0500

    Misc styling adjustments.

--- app/overrides/shared/change_products.rb
+++ app/overrides/shared/change_products.rb
@@ -3,7 +3,7 @@ Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :replace => "[data-hook='products_list_item']",
                      :name => "change_products",
                      :text => %q{
-    <li id="product_<%= product.id %>" class="columns three <%= cycle("", "omega", :name => "classes") %>" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
+    <li id="product_<%= product.id %>" class="columns four <%= cycle("", "omega", :name => "classes") %>" data-hook="products_list_item" itemscope itemtype="http://schema.org/Product">
       <div class="product-image">
         <%= link_to small_image(product, :itemprop => "image"), product, :itemprop => 'url' %>
       </div>
