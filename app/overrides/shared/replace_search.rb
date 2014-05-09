621aa1db7957c589567fe72a0f7d89daad61208a
ecnalyr <ecnalyr@gmail.com>
2014-05-09 06:08:18 -0500

    Replaced spree translation with literal.

--- app/overrides/shared/replace_search.rb
+++ app/overrides/shared/replace_search.rb
@@ -2,8 +2,8 @@ Deface::Override.new(:virtual_path  => "spree/shared/_nav_bar",
                      :replace => '#search-bar',
                      :name          => "replace_search",
                      :text => %q{
-    <li id="home-link" data-hook><%= link_to Spree.t(:shop), spree.root_path %></li>
-    <li id="about-link" data-hook><%= link_to Spree.t(:about), '/about' %></li>
-    <li id="about-link" data-hook><%= link_to Spree.t(:contact), '/contact-us' %></li>
+    <li id="home-link" data-hook><%= link_to 'Shop', spree.root_path %></li>
+    <li id="about-link" data-hook><%= link_to 'About, '/about' %></li>
+    <li id="about-link" data-hook><%= link_to 'Contact', '/contact-us' %></li>
     <li id="link-to-cart" data-hook><%= link_to_cart %></li>
                                 })
