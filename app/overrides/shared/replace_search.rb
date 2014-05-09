24132f42dd20cacb7463cb610845cd10bdd39167
ecnalyr <ecnalyr@gmail.com>
2014-05-09 06:40:12 -0500

    Added missing apostrophe.

--- app/overrides/shared/replace_search.rb
+++ app/overrides/shared/replace_search.rb
@@ -3,7 +3,7 @@ Deface::Override.new(:virtual_path  => "spree/shared/_nav_bar",
                      :name          => "replace_search",
                      :text => %q{
     <li id="home-link" data-hook><%= link_to 'Shop', spree.root_path %></li>
-    <li id="about-link" data-hook><%= link_to 'About, '/about' %></li>
+    <li id="about-link" data-hook><%= link_to 'About', '/about' %></li>
     <li id="about-link" data-hook><%= link_to 'Contact', '/contact-us' %></li>
     <li id="link-to-cart" data-hook><%= link_to_cart %></li>
                                 })
